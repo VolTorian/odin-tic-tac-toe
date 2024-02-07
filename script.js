072a75b0ad09be5103d53a02c9aea97347a96f51
VolTorian <33360939+VolTorian@users.noreply.github.com>
2024-02-07 15:27:52 -0800

    playerTurn checks if position is open

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
 const ticTacToeGame = (function () {
-    const gameBoard = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]];
+    const gameBoard = [[".", ".", "."], [".", ".", "."], [".", ".", "."]];
+    let playerState = "x";
+    let turns = 0;
 
     const displayBoard = () => {
+        console.log("  1 2 3");
         for (let i = 0; i < 3; i++) {
-            console.log(gameBoard[i].join(" "));
+            console.log(`${i + 1} ${gameBoard[i].join(" ")}`);
         }
     };
 
-    return {displayBoard};
+    const playerTurn = () => {
+        let row, col;
+        console.log(`${playerState}'s turn. Choose a row and column.`);
+
+        while (true) {
+            row = prompt("Enter a row number:");
+            col = prompt("Enter a column number:");
+
+            if (gameBoard[row - 1][col - 1] === ".") {
+                break;
+            }
+            console.log("Position already taken. Try again.");
+        }
+        gameBoard[row - 1][col - 1] = playerState;
+
+        if (playerState === "x") {
+            playerState = "o";
+        }
+        else {
+            playerState = "x";
+        }
+        turns++;
+        displayBoard();
+    };
+
+    return {displayBoard, playerTurn};
 })();
