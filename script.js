579fd94db8f448ff8226d54298e976759dd666b9
VolTorian <33360939+VolTorian@users.noreply.github.com>
2024-02-08 14:13:05 -0800

    function to check for a winner
    checks horizontal, vertical, diagonal, antidiagonal

--- script.js
+++ script.js
@@ -15,15 +15,17 @@ const ticTacToeGame = (function () {
         console.log(`${playerState}'s turn. Choose a row and column.`);
 
         while (true) {
-            row = prompt("Enter a row number:");
-            col = prompt("Enter a column number:");
+            row = prompt("Enter a row number:") - 1;
+            col = prompt("Enter a column number:") - 1;
 
-            if (gameBoard[row - 1][col - 1] === ".") {
+            if (gameBoard[row][col] === ".") {
                 break;
             }
             console.log("Position already taken. Try again.");
         }
-        gameBoard[row - 1][col - 1] = playerState;
+        gameBoard[row][col] = playerState;
+
+        checkWinner(row, col);
 
         if (playerState === "x") {
             playerState = "o";
@@ -33,6 +35,52 @@ const ticTacToeGame = (function () {
         }
         turns++;
         displayBoard();
+    };
+
+    const checkWinner = (row, col) => {
+        for (let i = 0; i < 3; i++) {
+            if (gameBoard[row][i] !== playerState) {
+                break;
+            }
+            if (i == 2) {
+                console.log(`${playerState} wins`);
+                return true;
+            }
+        }
+
+        for (let i = 0; i < 3; i++) {
+            if (gameBoard[i][col] !== playerState) {
+                break;
+            }
+            if (i == 2) {
+                console.log(`${playerState} wins`);
+                return true;
+            }
+        }
+
+        if (row === col) {
+            for (let i = 0; i < 3; i++) {
+                if (gameBoard[i][i] !== playerState) {
+                    break;
+                }
+                if (i == 2) {
+                    console.log(`${playerState} wins`);
+                    return true;
+                }
+            }
+        }
+        
+        if (row + col === 2) {
+            for (let i = 0; i < 3; i++) {
+                if (gameBoard[i][2 - i] !== playerState) {
+                    break;
+                }
+                if (i == 2) {
+                    console.log(`${playerState} wins`);
+                    return true;
+                }
+            }
+        }
     };
 
     return {displayBoard, playerTurn};
